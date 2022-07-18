3f3d3e794237aa7d52f33491170136116a14aa4a
Luca Rossi <lucarossi147@gmail.com>
2022-07-18 12:25:12 +0200

    modified ebike.ts

--- ebike/ebike.ts
+++ ebike/ebike.ts
@@ -10,6 +10,8 @@ interface EBike {
     pressureFrontWheel: number;
     pressureBackWheel: number;
     altitude: number;
+    availability: Availability;
+    maintenanceNeeded: boolean;
 }
 
 interface Position {
@@ -17,11 +19,42 @@ interface Position {
     lng: number;
 }
 
+enum Availability {
+    Available,
+    Unavailable,
+    Booked,
+}
+
 let eBike: EBike
 const servient = new Servient();
 
 servient.addServer(new HttpServer());
 
+function availabilityAsString(availability:Availability): String {
+    switch (availability) {
+        case Availability.Available:
+            return "available";
+        case Availability.Booked:
+            return "booked";
+        default:
+            return "unavailable";
+    }
+}
+function stringToAvailability(availability: String): Availability {
+    switch (availability){
+        case "available":
+            return Availability.Available;
+        case "booked":
+            return Availability.Booked;
+        default:
+            return Availability.Unavailable;
+    }
+}
+//TODO implement something more serious
+function notify(email, msg) {
+    console.log(email)
+    console.log(msg)
+}
 //actual implementation may differ from sensor to sensor
 function readFromSensor(upTo: number = 100){
     return Math.floor(Math.random()*upTo)
@@ -32,13 +65,7 @@ async function init(){
     // Then from here on you can use the WoT object to produce the thing
     // i.e WoT.produce({.....})
     let thing = await WoT.produce({
-        // '@context': [
-        //     'https://www.w3.org/2022/wot/td/v1.1',
-        //     { "saref": "https://w3id.org/saref#" }
-        // ],
-        // id: "cicciaculo",
         title: 'Smart eBike',
-        // '@type':'saref:EBike',
         description: 'A Smart eBike, equipped with several sensors',
         properties:{
             battery: {
@@ -46,6 +73,10 @@ async function init(){
                 description: 'Current battery level.',
                 minimum:0,
                 maximum:100,
+            },
+            maintenanceNeeded: {
+                type: "boolean",
+                description: 'True if maintenance is needed',
             },
             pressureFrontWheel: {
                 type: "number",
@@ -104,11 +135,23 @@ async function init(){
                         }
                     }
                 }
+            },
+            stop: {
+                description: 'Stopped to use the eBike',
+                output: {
+                    type: "number",
+                    description: 'returns the price of the ride',
+                    properties:{
+                        time:{
+                            type: "string",
+                        }
+                    }
+                }
             }
         },
         events:{
             lowBattery:{
-                description:'Battery is running low and need to be repalced',
+                description:'Battery is running low and need to be replaced',
                 data:{
                     type:"string",
                 }
@@ -122,7 +165,9 @@ async function init(){
         position: {lat : readFromSensor(), lng : readFromSensor()},
         pressureBackWheel: readFromSensor(10),
         pressureFrontWheel: readFromSensor(10),
-        upTime: readFromSensor()
+        upTime: readFromSensor(),
+        availability: Availability.Available,
+        maintenanceNeeded: false,
     }
 
     thing.setPropertyReadHandler("position", async () => eBike.position);
@@ -131,6 +176,16 @@ async function init(){
     thing.setPropertyReadHandler("pressureBackWheel", async () => eBike.pressureBackWheel);
     thing.setPropertyReadHandler("pressureFrontWheel", async () => eBike.pressureFrontWheel);
     thing.setPropertyReadHandler("altitude", async () => eBike.altitude);
+    thing.setPropertyReadHandler("availability", async () => availabilityAsString(eBike.availability))
+    thing.setPropertyReadHandler("speedometer", async () => readFromSensor(35))
+
+    thing.setPropertyWriteHandler("availability", async (val, options) => {
+        eBike.availability = stringToAvailability(await val.value());
+        if (eBike.availability == Availability.Unavailable) {
+            notify("company@mail.com", "need to check this one")
+        }
+    })
+
 
     // Finally expose the thing
     thing.expose().then(() => {
